33f4cafc3d3434eea7d0893a539264a649281df4
Abelardo Vieira Mota <abevieiramota@gmail.com>
2017-08-13 16:24:28 -0300

    refatoração(pq n foi junto no primeiro commit?)

--- src/main/java/br/com/abevieiramota/model/dao/ResultadoDAO.java
+++ src/main/java/br/com/abevieiramota/model/dao/ResultadoDAO.java
@@ -7,31 +7,34 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
 
+import br.com.abevieiramota.gui.wmain.Configuracao;
+import br.com.abevieiramota.messages.Messages;
+import br.com.abevieiramota.model.Premio;
 import br.com.abevieiramota.model.Resultado;
-import br.com.abevieiramota.model.Resultado.Premio;
 import br.com.abevieiramota.model.Resultado.ResultadoBuilder;
-import br.com.abevieiramota.model.Resultado.Turno;
+import br.com.abevieiramota.model.TipoLoteria;
+import br.com.abevieiramota.model.Turno;
 import br.com.abevieiramota.service.parser.Patterns;
 
-public class ResultadoDAO {
+public class ResultadoDao {
 
 	private Connection connection;
 
-	private final static String INSERT_TEMPLATE = "insert into resultado values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-	private final static String SELECT_ALL = "select * from resultado";
-	private final static String SELECT_BY_DATA = "select * from resultado where data = ? order by turno asc";
-	private final static String SELECT_ALL_DIURNO = "select * from resultado where turno = 0";
-	private final static String SELECT_ALL_NOTURNO = "select * from resultado where turno = 1";
+	private final static String INSERT_TEMPLATE = Messages.getString("sql.resultado.insert");
+	private final static String SELECT_ALL = Messages.getString("sql.resultado.all_tipo_turno");
+	private final static String SELECT_ALL_ = Messages.getString("sql.resultado.all.tipo");
+	private final static String SELECT_BY_DATA = Messages.getString("sql.resultado.data_tipo");
 
-	public ResultadoDAO() throws SQLException {
+	public ResultadoDao() throws SQLException {
 		connection = ConnectionFactory.instance();
 	}
 
 	public void salvar(Resultado resultado) throws SQLException {
+		checkNotNull(resultado);
 
 		try (PreparedStatement stmt = connection.prepareStatement(INSERT_TEMPLATE)) {
 			stmt.setQueryTimeout(30);
@@ -44,6 +47,7 @@ public class ResultadoDAO {
 
 	// TODO: melhorar
 	public void salvar(List<Resultado> resultados) throws SQLException {
+		checkNotNull(resultados);
 
 		for (Resultado resultado : resultados) {
 			try (PreparedStatement stmt = this.connection.prepareStatement(INSERT_TEMPLATE)) {
@@ -56,12 +60,44 @@ public class ResultadoDAO {
 		}
 	}
 
-	public List<Resultado> all() throws SQLException {
+	public List<Resultado> all(EnumSet<Turno> turnos, TipoLoteria tipo) throws SQLException {
+		checkNotNull(turnos);
+		checkNotNull(tipo);
+
+		if (EnumSet.allOf(Turno.class).equals(turnos)) {
+			return _all(tipo);
+		}
+
 		List<Resultado> resultados = new ArrayList<>();
-		try (Statement stmt = this.connection.createStatement()) {
+		try (PreparedStatement stmt = this.connection.prepareStatement(SELECT_ALL)) {
 			stmt.setQueryTimeout(30);
 
-			try (ResultSet rs = stmt.executeQuery(SELECT_ALL)) {
+			stmt.setInt(1, tipo.ordinal());
+			// FIXME: pressupõe que há apenas um elemento
+			stmt.setInt(2, turnos.iterator().next().ordinal());
+
+			try (ResultSet rs = stmt.executeQuery()) {
+				while (rs.next()) {
+					Resultado resultado = resultadoDoResultSet(rs);
+
+					resultados.add(resultado);
+				}
+			}
+		}
+
+		return resultados;
+	}
+
+	private List<Resultado> _all(TipoLoteria tipo) throws SQLException {
+		checkNotNull(tipo);
+
+		List<Resultado> resultados = new ArrayList<>();
+		try (PreparedStatement stmt = this.connection.prepareStatement(SELECT_ALL_)) {
+			stmt.setQueryTimeout(30);
+
+			stmt.setInt(1, tipo.ordinal());
+
+			try (ResultSet rs = stmt.executeQuery()) {
 				while (rs.next()) {
 					Resultado resultado = resultadoDoResultSet(rs);
 
@@ -75,50 +111,24 @@ public class ResultadoDAO {
 
 	public List<Resultado> allDiurno() throws SQLException {
 
-		List<Resultado> resultados = new ArrayList<>();
-
-		try (Statement stmt = connection.createStatement()) {
-			stmt.setQueryTimeout(30);
-
-			try (ResultSet rs = stmt.executeQuery(SELECT_ALL_DIURNO)) {
-				while (rs.next()) {
-					Resultado resultado = resultadoDoResultSet(rs);
-
-					resultados.add(resultado);
-				}
-			}
-		}
-
-		return resultados;
+		return all(EnumSet.of(Turno.DIURNO), Configuracao.getTipoLoteria());
 	}
 
 	public List<Resultado> allNoturno() throws SQLException {
 
-		List<Resultado> resultados = new ArrayList<>();
-
-		try (Statement stmt = connection.createStatement()) {
-			stmt.setQueryTimeout(30);
-
-			try (ResultSet rs = stmt.executeQuery(SELECT_ALL_NOTURNO)) {
-				while (rs.next()) {
-					Resultado resultado = resultadoDoResultSet(rs);
-
-					resultados.add(resultado);
-				}
-			}
-		}
-
-		return resultados;
+		return all(EnumSet.of(Turno.NOTURNO), Configuracao.getTipoLoteria());
 	}
 
-	public List<Resultado> byData(String data) throws SQLException {
+	public List<Resultado> byData(String data, TipoLoteria tipo) throws SQLException {
 		checkNotNull(data);
+		checkNotNull(tipo);
 		checkArgument(Patterns.REGEX_DATA.matcher(data).find());
 
 		List<Resultado> resultadosDoDia = new ArrayList<>(2);
 
-		try (PreparedStatement stmt = connection.prepareStatement(SELECT_BY_DATA)) {
+		try (PreparedStatement stmt = this.connection.prepareStatement(SELECT_BY_DATA)) {
 			stmt.setString(1, data);
+			stmt.setInt(2, tipo.ordinal());
 			stmt.setMaxRows(2);
 			stmt.setQueryTimeout(30);
 
@@ -157,13 +167,14 @@ public class ResultadoDAO {
 
 	private void preencheStatement(PreparedStatement stmt, Resultado resultado) throws SQLException {
 
-		stmt.setString(1, resultado.data);
+		stmt.setString(1, resultado.getData());
 
 		for (int i = 2; i <= 11; i++) {
 			stmt.setString(i, resultado.premio(Premio.values()[i - 2]));
 		}
 
-		stmt.setInt(12, resultado.turno.ordinal());
+		stmt.setInt(12, resultado.getTurno().ordinal());
+		stmt.setInt(13, resultado.getTipoLoteria().ordinal());
 	}
 
 }
